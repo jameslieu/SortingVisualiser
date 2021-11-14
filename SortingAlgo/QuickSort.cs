612bef2802ff3bb0be71a268d244c1bd684d8b47
James Lieu <j.lieu888@gmail.com>
2021-11-14 21:27:43 +0000

    initial commit
    - add RandArrayGenerator to get numbers for tests
    
    - add Quick Sort

---
+++ SortingAlgo/QuickSort.cs
@@ -0,0 +1,39 @@
+﻿namespace SortingAlgo
+{
+    public class QuickSort
+    {
+        public static List<int> Sort(List<int> numbers, int left, int right)
+        {
+            int i = left;
+            int j = right;
+
+            var pivot = numbers[(left + right) / 2];
+            while (i < j)
+            {
+                while (numbers[i] < pivot)
+                    i++;
+
+                while (numbers[j] > pivot)
+                    j--;
+
+                if (i <= j)
+                {
+                    var tmp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+                Sort(numbers, left, j);
+
+            if (i < right)
+                Sort(numbers, i, right);
+
+            return numbers;
+        }
+    }
+}
