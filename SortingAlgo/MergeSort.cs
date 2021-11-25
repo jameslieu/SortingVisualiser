34b9d3868fd0280ef05eefce23ec8c162f4fc909
James Lieu <j.lieu888@gmail.com>
2021-11-25 21:57:27 +0000

    Add merge sort algo with tests

---
+++ SortingAlgo/MergeSort.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SortingAlgo
+{
+    public class MergeSort
+    {
+        public static List<int> Sort(List<int> arr)
+        {
+            if (arr.Count <= 1)
+                return arr;
+
+            var left = new List<int>();
+            var right = new List<int>();
+
+            var middle = arr.Count / 2;
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(arr[i]);
+            }
+            for (int i = middle; i < arr.Count; i++)
+            {
+                right.Add(arr[i]);
+            }
+
+            left = Sort(left);
+            right = Sort(right);
+            return Merge(left, right);
+        }
+        
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            var result = new List<int>();
+            while(left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
