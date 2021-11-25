9c9706fc7a4defc70c28196d18d2c033255afa8d
James Lieu <j.lieu888@gmail.com>
2021-11-25 22:23:10 +0000

    Add insertion sort

---
+++ SortingAlgo/InsertionSort.cs
@@ -0,0 +1,23 @@
+﻿namespace SortingAlgo
+{
+    public class InsertionSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        int temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
