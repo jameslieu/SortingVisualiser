e200183b1463d14f5a001e4cf58aff3f7db9dd8b
James Lieu <j.lieu888@gmail.com>
2021-11-28 14:14:10 +0000

    Add selection sort

---
+++ SortingAlgo/SelectionSort.cs
@@ -0,0 +1,25 @@
+﻿namespace SortingAlgo
+{
+    public class SelectionSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            int temp, smallest;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+    }
+}
